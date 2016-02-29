9e3fcbec5aa8cf3756ea4e372dcf6a1a343da529
romainhild <romain.hild@me.com>
2016-02-29 13:14:14 +0000

    PS3EventClient inherits from CXBMCClient remove bind to Kodi use joystick map instead of event mapping (buttons start from 1)

--- include/PS3EventClient.h
+++ include/PS3EventClient.h
@@ -1,11 +1,12 @@
 #ifndef __PS3EVENTCLIENT_H__
 #define __PS3EVENTCLIENT_H__
 
+#include <fcntl.h> /* O_RDONLY O_NONBLOCKING */
 #include <linux/input.h>
 
 #include "xbmcclient.h"
 
-class PS3EventClient
+class PS3EventClient : public CXBMCClient
 {
 private:
     std::ofstream M_out;
@@ -13,27 +14,32 @@ private:
     int M_handler;
     std::string M_controllerName;
     std::string M_keymap;
-    CAddress M_addr;
-    int M_sockfd;
-    struct input_event M_event;
-    CPacketBUTTON M_button;
     bool M_hasGrabed;
 
 public:
-    PS3EventClient();
+    PS3EventClient(const char *IP = "127.0.0.1", int Port = 9777, int Socket = -1, unsigned int UID = 0) : CXBMCClient(IP, Port, Socket, UID)
+    {
+	M_out.open( "/home/xbian/ps3.out");
+	M_handlerName = "/dev/input/event3";
+	M_handler = open( M_handlerName.c_str(), O_RDONLY | O_NONBLOCK );
+	M_hasGrabed = false;
+	char name[128];
+	if (ioctl(M_handler, EVIOCGNAME(sizeof(name)), name) < 0)
+	    strncpy(name, "Unknown", sizeof(name));
+	M_controllerName = std::string(name);
+	M_keymap = "JS0:" + M_controllerName;
+        M_out << "PS3EventClient send to " << IP <<":" << Port << " using keymap " << M_keymap << std::endl;
+    }
     std::string handlerName() const { return M_handlerName; }
     void setHandlerName( std::string s ) { M_handlerName = s; }
     std::string controllerName() const { return M_controllerName; }
     std::string keymap() const { return M_keymap; }
     void setKeymap( std::string s ) { M_keymap = s; }
-    void setAddr( CAddress s ) { M_addr = s; }
     bool hasGrabed() const { return M_hasGrabed; }
     
     void grabDevice();
     void releaseDevice();
-    int connectToKodi();
     void readEvent();
-    void processEvent( input_event );
 };
 
 #endif
--- src/PS3EventClient.cpp
+++ src/PS3EventClient.cpp
@@ -1,4 +1,3 @@
-#include <fcntl.h> /* O_RDONLY O_NONBLOCKING */
 #include <sys/ioctl.h> /* ioctl */
 #include <unistd.h>
 
@@ -33,19 +32,6 @@ right horizontal : 2
 right vertical : 3
 */
 
-PS3EventClient::PS3EventClient( )
-{
-    M_out.open( "/home/xbian/ps3.out");
-    M_handlerName = "/dev/input/event3";
-    M_handler = open( M_handlerName.c_str(), O_RDONLY | O_NONBLOCK );
-    M_hasGrabed = false;
-    char name[128];
-    if (ioctl(M_handler, EVIOCGNAME(sizeof(name)), name) < 0)
-    	strncpy(name, "Unknown", sizeof(name));
-    M_controllerName = std::string(name);
-    M_keymap = "JS0:" + M_controllerName;
-}
-
 void PS3EventClient::grabDevice()
 {
     if ( ioctl(M_handler, EVIOCGRAB, 1) )
@@ -60,46 +46,33 @@ void PS3EventClient::releaseDevice()
     ioctl(M_handler, EVIOCGRAB, 0);
 }
 
-int PS3EventClient::connectToKodi()
-{
-    M_addr; // Address => localhost on 9777
-    M_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (M_sockfd < 0)
-    	return -1;
-
-    M_addr.Bind(M_sockfd);
-
-    CPacketHELO HeloPackage("PS3EventClient", ICON_NONE);
-    HeloPackage.Send(M_sockfd, M_addr);
-    M_out << "say hello" << std::endl;
-    return 0;
-}
-
 void PS3EventClient::readEvent()
 {
     struct input_event e;
-    int rc = read(M_handler, &e, sizeof(e));
-    if ( e.type == 1 )
-	M_out << "read event of type " << e.type << " for code " << e.code << " and value " << e.value << std::endl;
-    processEvent( e );
-    M_button.Send(M_sockfd, M_addr);
-}
-
-void PS3EventClient::processEvent( input_event e )
-{
     unsigned short flag;
-    switch ( e.type ) {
-	case 1:
-	    switch ( e.value ) {
-		case 0:
-		    flag = 0x04;
-		case 1:
-		    flag = 0x02;
-		default:
-		    break;
-		}
-	    M_button = CPacketBUTTON( (int)e.code, M_keymap.c_str(), flag );
-	default:
-	    break;
-	}
+    unsigned short code;
+    int rc = read(M_handler, &e, sizeof(e));
+    if ( rc > 0 )
+    {
+        if ( e.type == 1 )
+            M_out << "read event of type " << e.type << " for code " << e.code << " and value " << e.value << std::endl;
+        switch ( e.type ) {
+        case 1:
+            switch ( e.value ) {
+            case 0:
+                flag = BTN_UP;
+                break;
+            case 1:
+                flag = BTN_DOWN;
+                break;
+            default:
+                break;
+            }
+            code = e.code - 287;
+            SendButton(code, M_keymap.c_str(), flag);
+            break;
+        default:
+            break;
+        }
+    }
 }
